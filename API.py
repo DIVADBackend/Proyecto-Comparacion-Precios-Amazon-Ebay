f0b6172ee3564d83b5eb1809d7dac746b1ff9ab6
DIVADBackend <valenciapadillajuandavid@gmail.com>
2025-01-14 14:30:13 -0500

    Usando la API Real-Time Amazon Data para obtener el precio de un producto

---
+++ API.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+url = "https://real-time-amazon-data.p.rapidapi.com/product-details"
+
+querystring = {"asin":"B0CQ4GYTTX", "country": "US"}
+
+headers = {
+    "x-rapidapi-key": "7eff510885msh1aebe1be7a09d6ap11dcdajsn1a6b5ac05529",
+	"x-rapidapi-host": "real-time-amazon-data.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+json_response = response.json()
+
+data = json_response['data']
+
+
+print(data['product_price'])
